14245bcff01b8484cda6ad1cfe885045dc71e579
Edward Lale <edward.gratefuldead1@gmail.com>
2025-11-22 23:20:45 -0600

    Add GSAP animations and dependencies for various components

--- src/pages/GsapText.jsx
+++ src/pages/GsapText.jsx
@@ -1,6 +1,27 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapText = () => {
   // TODO: Implement gsap text animation
 
+  useGSAP(() => {
+    gsap.to('#text', {
+      ease: "power1.out",
+      opacity: 1,
+      y: 0
+    })
+    gsap.fromTo('.para', {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+      ease: "power1.out",
+    }, {
+      opacity: 1,
+      y: 0,
+      delay: 0.1,
+      stagger: 0.2
+    })
+  })
   return (
     <main>
       <h1 id="text" className="opacity-0 translate-y-10">
